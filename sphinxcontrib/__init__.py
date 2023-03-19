3267acb8a2dab660e1a0dd7099441ddf5268a77d
Benedikt Magnusson <bsm@hi.is>
2023-03-19 14:18:25 +0000

    rename

--- sphinxcontrib/__init__.py
+++ sphinxcontrib/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-name = 'eqt_ext'
-
+# name = 'eqt_ext'
+__import__("pkg_resources").declare_namespace(__name__)
 
 def get_eqt_ext_static_dir():
     return os.path.join(os.path.dirname(__file__), '_static')
