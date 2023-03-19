5198e54938b000eac57c05c651a5b7f5d9f8ab50
Benedikt Magnusson <bsm@hi.is>
2023-03-19 14:42:36 +0000

    rename

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sphinxcontrib-questionnaire',
-    version='2.0',
+    version='2.0.3',
     author='Naeka',
     author_email='contact@naeka.fr',
     maintainer="Benedikt Magnusson",
